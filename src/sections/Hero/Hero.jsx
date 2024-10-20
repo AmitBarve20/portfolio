3cdb5e0b87a36aaa13c929edd78be79af07be01c
Amit <amitbarve2003@gmail.com>
2024-10-20 17:50:53 +0530

    All changes

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -8,16 +8,22 @@ import githubLight from '../../assets/github-light.svg';
 import githubDark from '../../assets/github-dark.svg';
 import linkedinLight from '../../assets/linkedin-light.svg';
 import linkedinDark from '../../assets/linkedin-dark.svg';
+import behance from '../../assets/behance.svg'
+import behanceDark from '../../assets/behanceDark.svg'
+
+
 import CV from '../../assets/AB_resume.pdf';
 import { useTheme } from '../../common/ThemeContext';
 
 function Hero() {
   const { theme, toggleTheme } = useTheme();
-
+  
+  // const behance = theme === 'light' ? behance : behanceDark;
   const themeIcon = theme === 'light' ? sun : moon;
   const twitterIcon = theme === 'light' ? twitterLight : twitterDark;
   const githubIcon = theme === 'light' ? githubLight : githubDark;
   const linkedinIcon = theme === 'light' ? linkedinLight : linkedinDark;
+
 
   return (
     <section id="hero" className={styles.container}>
@@ -52,6 +58,9 @@ function Hero() {
           </a>
           <a href="https://linkedin.com/" target="_blank">
             <img src={linkedinIcon} alt="Linkedin icon" />
+          </a>
+          <a href="https://behance.net/amitbarve" target="_blank">
+            <img src={behance} alt="behance icon" />
           </a>
         </span>
         <p className={styles.description}>
--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -1,8 +1,8 @@
 import styles from './ProjectsStyles.module.css';
-import viberr from '../../assets/banking app.webp';
+import banking from '../../assets/banking_.png';
 import musicapp from '../../assets/musicapp.webp';
-import hipsster from '../../assets/hipsster.png';
-import fitLift from '../../assets/fitlift.png';
+import webiste from '../../assets/website.webp';
+import crypto from '../../assets/crypto.webp';
 import ProjectCard from '../../common/ProjectCard';
 
 function Projects() {
@@ -11,7 +11,7 @@ function Projects() {
       <h1 className="sectionTitle">Projects</h1>
       <div className={styles.projectsContainer}>
         <ProjectCard
-          src={viberr}
+          src={banking}
           link="https://www.figma.com/design/ARfR0hx9dA0IoU5vD2nnfY/Banking-app?node-id=0-1&t=kWC0mSW2C7EwcRsO-1"
           h3="Cashflow"
           p="Digital Banking App"
@@ -20,19 +20,19 @@ function Projects() {
           src={musicapp}
           link="https://www.figma.com/design/vWs5RYrpQYrlUQ2VnQVwjr/Music-App?node-id=0-1&t=nkHc3BWXG2N6Itvy-1"
           h3="Music app"
-          p="Hamburger Restaurant"
+          p="Morden Bhakti"
         />
         <ProjectCard
-          src={fitLift}
+          src={crypto}
           link="https://www.figma.com/design/3gFcwGHvbx1r5FY8ocNwng/dashboard?node-id=1-3578&t=0xqtXtrBTGAKqpVw-1"
-          h3="dashboard"
-          p="Glasses Shop"
+          h3="Dashboard"
+          p="Simply Manage Everything"
         />
         <ProjectCard
-          src={hipsster}
+          src={webiste}
           link="https://www.figma.com/design/7Id9DgeSkeNJmpflEizKnw/landing-page?node-id=1-2469&t=QyBULgq11w9hYp8H-1"
-          h3="landing page"
-          p="Glasses Shop"
+          h3="Landing page"
+          p="Buy drinks online"
         />
       </div>
     </section>
